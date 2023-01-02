b161ec7f3b9b5f0fffad0dfb9e4ea815ca166969
MaxEnglish <maxjenglish@gmail.com>
2023-01-02 15:04:34 -0500

    more wordle work

--- src/Wordle/components-wordle/letter-space.js
+++ src/Wordle/components-wordle/letter-space.js
@@ -3,32 +3,11 @@ import '../css-wordle/tile-space.css'
 
 export default function LetterSpace (props) {
 
-    const letters = /^[A-Za-z]+$/;
-
     return (
         <input
         type='text'
         className='letter-space'
         id = {props.index}
-        onInput={(e)=> {
-            let target = e.target.value;
-         
-            if (target.match(letters)) {
-                e.target.value = target.toUpperCase();
-
-                if (target.length > 1) {
-                    target = target.slice(-1);
-                    e.target.value = target.toUpperCase();
-                }
-
-                const nextSpace = document.getElementById(props.index + 1);
-                if (nextSpace) {
-                    nextSpace.select();
-                }
-            } else {
-                e.target.value = '';  
-            }
-        }}
         ></input>
     )
 }
--- src/pages/WordleGame.js
+++ src/pages/WordleGame.js
@@ -3,29 +3,98 @@ import LetterSpace from '../Wordle/components-wordle/letter-space';
 
 export default function WordleGame () {
 
-    const [iteration, setIteration] = useState(0);
+    const letters = /^[A-Za-z]{1}$/;
+    const endPoints = [3,7,11,15];
+    const startPoints = [0,4,8,12];
+    var inFetch = false;
+    var theWord = 'FREE';
 
-    const letters = /^[A-Za-z]+$/;
+    const getValueOfRow = (rowNum) => {
+        let finalString = '';
+        Array.from(document.getElementById(rowNum).children).forEach((space) => {
+            finalString += space.value;
+        });
+        return finalString.toLowerCase();
+    }
 
     useEffect(()=> {
-        document.addEventListener('keydown', (e)=> {
-            if (e.key === 'Backspace' && e.target.tagName === "INPUT") {
-                e.preventDefault()
-                if (e.target.value)
-                    e.target.value = '';
-                if (e.target.id !== '0'){
-                    document.getElementById(e.target.id - 1).select();
+        document.addEventListener('keydown', async (e)=> {
+            e.preventDefault();
+            const id = parseInt(e.target.id,10);
+
+            if (!inFetch) {
+                //user press backspace
+                if (e.key === 'Backspace') {
+                    if (e.target.value)
+                        e.target.value = '';
+                    if (!startPoints.includes(id)){
+                        document.getElementById(id - 1).select();
+                    }
+                //user press a letter
+                } else if (e.key.match(letters)) {
+                    document.getElementById(id).value = e.key.toUpperCase();
+                    if (!endPoints.includes(id))
+                        document.getElementById(id + 1).select();
+                //user press Enter
+                } else if (e.key === "Enter" && endPoints.includes(id) && e.target.value.match(letters)) {
+                    inFetch = true;
+
+                    const word = getValueOfRow(e.target.parentElement.id);
+
+                    await fetch('https://api.wordnik.com/v4/word.json/' + word + '/scrabbleScore?api_key=ewnok8gtygsnov62psuoiwe6qckl8qfpzxa4wue8nhk14468l')
+                    .then((response) => response.json())
+                    .then((data) => {
+                        const spaces = Array.from(e.target.parentElement.children);
+                        if (!data.value) {
+                            spaces.forEach((space) => {
+                                space.style.animation = 'shake .5s'
+                            });
+                            
+                        } else {
+                            const word = [...theWord];
+
+                            spaces.forEach((space, index) => {
+                                const val = space.value;
+                                setTimeout(() => {
+                                    if (val === word[index]) {
+                                        space.classList.add('my-green')
+                                    } else if (word.includes(val)) {
+                                        space.classList.add('my-yellow')
+                                    } else {
+                                        space.classList.add('my-gray')
+                                    }
+                                }, index * 800) 
+                            })
+                            if (id !== 15)
+                                document.getElementById(parseInt(id,10) + 1).select();
+                        }
+                        setTimeout(() => {
+                            spaces.forEach((space) => {
+                                space.style = null;
+                            })
+                        },501);
+                        
+                    })
+                    .catch((err) => { 
+                        console.log(err);
+                    })
+
+                    inFetch = false;
+                    
                 }
             }
-        });
+        }); 
 
-        
+        document.getElementById(0).select();
+
+        document.addEventListener('mousedown', (e) => e.preventDefault())
     },[])
 
     return (
         <>
+            <header>FREE</header>
             {[...Array(4)].map((row, rowNum) => (
-                <div className='wordle-row' key={rowNum}>
+                <div className='wordle-row' key={'row-' + rowNum} id={'row-' + rowNum}>
                 {[...Array(4)].map((space, i) => (
                     <LetterSpace key = {i} index = {rowNum * 4 + i}/>
                 ))}
