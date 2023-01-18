94c60eae80242c164f97fb7a5a614216a333f903
MaxEnglish <maxjenglish@gmail.com>
2023-01-17 21:04:06 -0500

    navbar padding issue

--- src/Navbar.js
+++ src/Navbar.js
@@ -5,10 +5,10 @@ export default function Navbar(){
         <nav className="fixed top-0 left-0 w-full bg-white shadow-sm px-8 md:px-20 lg:px-40 flex justify-between items-center z-50">
             <h1 className="font-burtons text-lg">Max English Personal Website</h1>
             <ul className="flex items-center">
-                <h1 className='hover:bg-sky-50 py-5 px-10 cursor-pointer' onClick={()=> document.getElementById('portfolio').scrollIntoView({behavior: 'smooth', block: 'center'})}>Projects</h1>
-                <h1 className='hover:bg-sky-50 py-5 px-10 cursor-pointer' onClick={()=> document.getElementById('experience').scrollIntoView({behavior: 'smooth', block: 'center'})}>Experience</h1>
-                <h1 className='hover:bg-sky-50 py-5 px-10 cursor-pointer' onClick={()=> document.getElementById('expertise').scrollIntoView({behavior: 'smooth', block: 'center'})}>Expertise</h1>
-                <h1 className='mr-20 hover:bg-sky-50 py-5 px-10 cursor-pointer' onClick={()=> document.getElementById('about-me').scrollIntoView({behavior: 'smooth', block: 'center'})}>About Me</h1>
+                <h1 className='hover:bg-sky-50 cursor-pointer navbar-padding' onClick={()=> document.getElementById('portfolio').scrollIntoView({behavior: 'smooth', block: 'center'})}>Projects</h1>
+                <h1 className='hover:bg-sky-50 cursor-pointer navbar-padding' onClick={()=> document.getElementById('experience').scrollIntoView({behavior: 'smooth', block: 'center'})}>Experience</h1>
+                <h1 className='hover:bg-sky-50cursor-pointer navbar-padding' onClick={()=> document.getElementById('expertise').scrollIntoView({behavior: 'smooth', block: 'center'})}>Expertise</h1>
+                <h1 className='hover:bg-sky-50 cursor-pointer navbar-padding right-navbar-padding' onClick={()=> document.getElementById('about-me').scrollIntoView({behavior: 'smooth', block: 'center'})}>About Me</h1>
                 <Link to='/resume' target="_blank">
                     <div className="px-4 py-2 bg-gradient-to-r from-cyan-500 text- to-teal-500 bg-blue-300 rounded-md ml-8 text-white">Resume</div>
                 </Link>
